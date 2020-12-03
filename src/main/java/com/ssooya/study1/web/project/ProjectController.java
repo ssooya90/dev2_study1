c8cc044e9eb555c0d8eab8f68bb48ae879d67832
ssooya <ssooya90@naver.com>
2020-12-03 09:08:26 +0900

    Feat url 맵핑

--- src/main/java/com/ssooya/study1/web/project/ProjectController.java
+++ src/main/java/com/ssooya/study1/web/project/ProjectController.java
@@ -2,13 +2,26 @@ package com.ssooya.study1.web.project;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class ProjectController {
+
 	@GetMapping("/project/list")
-	public String projectList(){
+	public String projectList(Model model){
+		model.addAttribute("currentNum","01");
 		return "/project/project_list";
+	}
+
+	@GetMapping("/project/reg")
+	public String projectReg(){
+		return "/project/project_reg";
+	}
+
+	@GetMapping("/project/edit")
+	public String projectEdit(){
+		return "/project/project_edit";
 	}
 }
